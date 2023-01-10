95b9eab5b4bd32c4eb86c1f035ec510c1beff551
Rafael Moraes <rafaelsm003@gmail.com>
2023-01-10 14:47:25 -0300

    fix: muda o formato das datas no teste da classe Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -6,18 +6,22 @@ def test_cria_produto():
         id=1,
         nome_da_empresa="Nestlé",
         nome_do_produto="Chocolate",
-        data_de_fabricacao="01-01-2023",
-        data_de_validade="01-01-2025",
+        data_de_fabricacao="2023-01-01",
+        data_de_validade="2025-01-01",
         numero_de_serie="12345678",
         instrucoes_de_armazenamento="Consume within 2 days of opening.",
     )
 
+    # SUCCESS
     assert (chocolate.id) == 1
     assert (chocolate.nome_da_empresa) == "Nestlé"
     assert (chocolate.nome_do_produto) == "Chocolate"
-    assert (chocolate.data_de_fabricacao) == "01-01-2023"
-    assert (chocolate.data_de_validade) == "01-01-2025"
+    assert (chocolate.data_de_fabricacao) == "2023-01-01"
+    assert (chocolate.data_de_validade) == "2025-01-01"
     assert (chocolate.numero_de_serie) == "12345678"
     assert (
         chocolate.instrucoes_de_armazenamento
     ) == "Consume within 2 days of opening."
+
+    assert (chocolate.data_de_fabricacao) != "01/01/2023"
+    assert (chocolate.data_de_validade) != "01/01/2025"
