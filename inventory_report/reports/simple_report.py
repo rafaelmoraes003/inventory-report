41a2eabb7cff0134f3881dcdbc673ea5011cfb9f
Rafael Moraes <rafaelsm003@gmail.com>
2023-01-10 14:48:34 -0300

    feat: implementa o método estático nearest_date_from_today, que recebe uma lista de dats e retorna a data mais próxima a partir de hoje

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,21 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def nearest_date_from_today(dates: list[str]):
+        dates_from_today = []
+
+        for date in dates:
+            [year, month, day] = date.split("-")
+            today = datetime.now()
+            curr_date = datetime(int(year), int(month), int(day))
+            date_diff = (curr_date - today).days
+            if date_diff >= 0:
+                dates_from_today.append(
+                    datetime.strftime(curr_date, "%Y-%m-%d")
+                )
+
+        return min(dates_from_today)
+
