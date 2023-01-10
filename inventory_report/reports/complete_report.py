48d7aa6879509b6ffa1526986a044ec88f59c064
Rafael Moraes <rafaelsm003@gmail.com>
2023-01-10 18:27:50 -0300

    feat: implementa o método get_companies_occurrences na classe CompleteReport, que tem o objetivo de criar um dicionário com a quantidade de aparições de empresas em uma lista

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -2,4 +2,15 @@ from inventory_report.reports.simple_report import SimpleReport
 
 
 class CompleteReport(SimpleReport):
-    pass
+    @staticmethod
+    def get_companies_occurrences(products_list: list[dict]):
+        products_by_company = {}
+
+        for product in products_list:
+            if product["nome_da_empresa"] in products_by_company:
+                products_by_company[product["nome_da_empresa"]] += 1
+            else:
+                products_by_company[product["nome_da_empresa"]] = 1
+
+        return products_by_company
+
