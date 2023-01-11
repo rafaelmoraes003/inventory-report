972c1fdbce29a1b4abf852261267a6d28b417047
Rafael Moraes <rafaelsm003@gmail.com>
2023-01-11 19:40:41 -0300

    feat: cria testes para o método __repr__ da classe Product

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    coffee = Product(
+        id=1,
+        nome_do_produto="Coffee",
+        nome_da_empresa="Nature Coffees",
+        data_de_fabricacao="2020-07-04",
+        data_de_validade="2023-02-09",
+        numero_de_serie="FR48",
+        instrucoes_de_armazenamento="In a cool and dry place",
+    )
+
+    coffee_data = str(coffee)
+
+    assert (isinstance(coffee_data, str)) is True
+    assert (coffee_data) == (
+        "O produto Coffee fabricado em 2020-07-04 por Nature Coffees "
+        "com validade até 2023-02-09 precisa ser "
+        "armazenado In a cool and dry place."
+    )
