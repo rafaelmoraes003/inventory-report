6c4533e72b798e460658747d880812c8fab8d052
Rafael Moraes <rafaelsm003@gmail.com>
2023-01-11 18:14:26 -0300

    feat: cria o método read_data na classe Inventory para ler arquivos csn, json e xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,26 +1,23 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
+import xmltodict
 
-
-reports = {
-    "simples": SimpleReport,
-    "completo": CompleteReport
+actions_by_file_extension = {
+    "csv": lambda file: list(
+        csv.DictReader(file, delimiter=",", quotechar='"')
+    ),
+    "json": lambda file: json.load(file),
+    "xml": lambda file: xmltodict.parse(file.read())["dataset"]["record"],
 }
 
 
 class Inventory:
     @staticmethod
-    def open_csv(file_path, report_type):
+    def read_data(file_path):
+        extension = file_path.split(".")[1]
         with open(file_path, mode="r") as file:
-            data = csv.DictReader(file, delimiter=',', quotechar='"')
-        if report_type == "simples":
-            return SimpleReport.generate(data)
-        else:
-            return CompleteReport.generate(data)
-
-    @classmethod
-    def import_data(cls, file_path, report_type):
-        # products_list = cls.open_csv(file_path)
-        # products_report = reports[report_type].generate(products_list)
-        return cls.open_csv(file_path, report_type)
+            extension = file_path.split(".")[1]
+            data = actions_by_file_extension[extension](file)
+        return data
