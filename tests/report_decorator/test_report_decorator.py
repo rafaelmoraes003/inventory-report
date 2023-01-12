e2a0f722ee13ab9b42a570cbfe747f74de0ff458
Rafael Moraes <rafaelsm003@gmail.com>
2023-01-12 00:22:14 -0300

    feat: cria testes para o método generate da classe ColoredReport, que gera um relatório colorido do estoque

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,44 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+from pytest import fixture
 
 
-def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+@fixture
+def product_data():
+    return [
+        {
+            "id": 1,
+            "nome_do_produto": "Cafe",
+            "nome_da_empresa": "Cafes Nature",
+            "data_de_fabricacao": "2020-07-04",
+            "data_de_validade": "2023-02-09",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "instrucao",
+        }
+    ]
+
+
+def test_decorar_relatorio(product_data):
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    BLUE = "\033[36m"
+
+    colored_report = ColoredReport(SimpleReport)
+    colored_text = colored_report.generate(product_data).split("\n")
+
+    titles = []
+    dates = []
+
+    for line in colored_text:
+        titles.append(line.split(":")[0])
+        dates.append(line.split(":")[1])
+        company = line.split(":")[1]
+    dates.pop()
+
+    for title in titles:
+        assert (GREEN in title) is True
+
+    for date in dates:
+        assert (BLUE in date) is True
+
+    assert (RED in company) is True
